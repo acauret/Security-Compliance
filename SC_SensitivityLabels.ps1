6eef03b7a7e31a7959e125ba26ab2815e377d4b5
Andrew and Geni Auret <acauret@hotmail.co.uk>
2019-11-07 12:22:27 +0000

    updates

--- SC_SensitivityLabels.ps1
+++ SC_SensitivityLabels.ps1
@@ -41,69 +41,127 @@
 [OutputType()]
 [CmdletBinding(DefaultParameterSetName)]
 Param (
-    [Parameter(Mandatory = $False, Position = 1, ParameterSetName = 'Credential')]
+    [Parameter(Mandatory = $False, Position = 0, ParameterSetName = 'Credential')]
     [PSCredential]$Credential,
-    [Parameter(Mandatory = $False, Position = 1, ParameterSetName = 'MFA')]
-    [Switch]$MFA
+
+    [Parameter(Mandatory = $False, Position = 0, ParameterSetName = 'MFA')]
+    [Switch]$MFA,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("get","test")]
+    [string]$Mode,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("Label","LabelPolicy")]
+    [string]$Type
+
 ) 
 
+
 #--------------------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------------
-$VerbosePreference = "Continue" 
-$scriptPath = $myInvocation.MyCommand.Path
-$scriptFolder = Split-Path $scriptPath
-#
-
-Write-Verbose "Checking for Common_Functions module..."
-
-$CommonModule = Get-Module -Name "Common_Functions" -ListAvailable
-
-if ($null -eq $CommonModule) {
-    Write-Verbose ""
-    Write-Verbose "Common_Functions Powershell module not installed..." 
-    Write-Verbose "Installing Common_Functions module" 
-    Write-Verbose ""
-    Import-Module $scriptFolder\Common_Functions.psd1 -Force -Verbose
-}
-Else{
-    Write-Verbose "Common_Functions Powershell module is installed"
-}
-#
-If ($MFA.IsPresent){
-    Initialize-Modules("CreateExoPSSession")
-}
-If (!($MFA.IsPresent))
-{
-    If (!($Credential.UserName)){
-        Write-Verbose "Gathering Credentials for non-MFA sign on"
-        $Credential = Get-Credential -Message "Please enter your Office 365 credentials"
+Process{
+    $VerbosePreference = "Continue" 
+    $scriptPath = $myInvocation.MyCommand.Path
+    $scriptFolder = Split-Path $scriptPath
+    #
+    
+    Write-Verbose "Checking for Common_Functions module..."
+    
+    $CommonModule = Get-Module -Name "Common_Functions" -ListAvailable
+    
+    if ($null -eq $CommonModule) {
+        Write-Verbose ""
+        Write-Verbose "Common_Functions Powershell module not installed..." 
+        Write-Verbose "Installing Common_Functions module" 
+        Write-Verbose ""
+        Import-Module $scriptFolder\Common_Functions.psd1 -Force -Verbose
     }
-}
-#
-If (!(Get-PSSession).ConfigurationName -eq "Microsoft.Exchange"){
+    Else{
+        Write-Verbose "Common_Functions Powershell module is installed"
+    }
+    #
+
     If ($MFA.IsPresent){
-        . $scriptFolder"\Exchange_Online_Module\CreateExoPSSession.ps1"
-        Connect-IPPSSession
-        Push-Location $scriptFolder
+        Initialize-Modules("CreateExoPSSession")
     }
-    else {
-        $newPSSessionSplat = @{
-            ConfigurationName = 'Microsoft.Exchange'
-            ConnectionUri   = "https://ps.compliance.protection.outlook.com/powershell-liveid/"
-            Authentication    = 'Basic'
-            Credential       = $Credential
-            AllowRedirection  = $true
+    If (!($MFA.IsPresent))
+    {
+        If (!($Credential.UserName)){
+            Write-Verbose "Gathering Credentials for non-MFA sign on"
+            $Credential = Get-Credential -Message "Please enter your Office 365 credentials"
         }
-        try{
-            $Session = New-PSSession @newPSSessionSplat -ErrorAction Stop
-            Write-Verbose "Connecting to Security & Compliance Center"
-            Import-PSSession $Session -AllowClobber -ErrorAction Stop
+    }
+    #
+    If (!(Get-PSSession).ConfigurationName -eq "Microsoft.Exchange"){
+        If ($MFA.IsPresent){
+            . $scriptFolder"\Exchange_Online_Module\CreateExoPSSession.ps1"
+            Connect-IPPSSession
+            Push-Location $scriptFolder
         }
-        catch{
-            Write-Output $_.Exception.Message
-            Write-Error "Please connect using Multi-Factor authentication instead using the -MFA switch"
-            break            
+        else {
+            $newPSSessionSplat = @{
+                ConfigurationName = 'Microsoft.Exchange'
+                ConnectionUri   = "https://ps.compliance.protection.outlook.com/powershell-liveid/"
+                Authentication    = 'Basic'
+                Credential       = $Credential
+                AllowRedirection  = $true
+            }
+            try{
+                $Session = New-PSSession @newPSSessionSplat -ErrorAction Stop
+                Write-Verbose "Connecting to Security & Compliance Center"
+                Import-PSSession $Session -AllowClobber -ErrorAction Stop
+            }
+            catch{
+                Write-Output $_.Exception.Message
+                Write-Error "Please connect using Multi-Factor authentication instead using the -MFA switch"
+                break            
+            }
+        }
+    }
+    #
+    switch ($Mode) {
+        "get" {
+            switch ($Type) {
+                "Label" {
+                    Write-Output "Getting the content of the current Sensitivity Labels"
+                    $labels = Get-Label
+                    foreach($label in $labels){
+                        $labelpolicyRule = Get-Labelpolicyrule | Where-Object {$_.LabelName -eq $label.Name}
+                        Write-Output "Name         : $($label.Name)"
+                        Write-Output "Created by   : $($label.CreatedBy)"
+                        Write-Output "Last modified: $($label.LastModifiedBy)"
+                        Write-Output "Display Name : $($label.DisplayName)"
+                        Write-Output "Tooltip      : $($label.Tooltip)"
+                        Write-Output "Description  : $($label.Comment)"
+                        $labelEncryptAction = ($labelpolicyRule | Where-Object {$_.LabelActionName -eq 'encrypt'}).LabelActionName
+                        If ($labelEncryptAction -eq "encrypt"){
+                            Write-Output "Encryption   : On"
+                        }
+                        else {
+                            Write-Output "Encryption   : Off"
+                        }
+                        Write-Output ""
+                    }
+
+                  }
+                "LabelPolicy" {
+
+                }
+            }
+        }
+        "test" {
+            switch ($Type) {
+                "Label" {
+
+                  }
+                "LabelPolicy" {
+                    
+                }
+            }
         }
     }
 }
+#
+    
 
